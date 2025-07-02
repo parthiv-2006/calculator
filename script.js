ef8fe8f929361c937ca903d005a8b2d243f7de9f
parthiv-2006 <parthiv.paul5545@gmail.com>
2025-07-02 13:20:28 -0400

    created the add, subtract, divide, and multiplication functions

---
+++ script.js
@@ -0,0 +1,24 @@
+let num1;
+let num2;
+let operation;
+
+
+function add(value, operationalValue) {
+    return value + operationalValue
+}
+
+function subtract(value, operationalValue) {
+    return value - operationalValue
+}
+
+function multiply(value, operationalValue) {
+    return value * operationalValue
+}
+
+function divide(value, operationalValue) {
+    return value / operationalValue
+}
+
+function operate(num1, operation, num2) {
+    
+}
