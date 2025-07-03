54db887a5e883691f1522438c6850a4a9b774dff
parthiv-2006 <parthiv.paul5545@gmail.com>
2025-07-03 15:54:10 -0400

    created references to all buttons for the calculator and added functionality for the clear all button

--- script.js
+++ script.js
@@ -2,6 +2,28 @@ let num1;
 let num2;
 let operation;
 
+const zeroButton = document.querySelector('#zero')
+const oneButton = document.querySelector('#one')
+const twoButton = document.querySelector('#two')
+const threeButton = document.querySelector('#three')
+const fourButton = document.querySelector('#four')
+const fiveButton = document.querySelector('#five')
+const sixButton = document.querySelector('#six')
+const sevenButton = document.querySelector('#seven')
+const eightButton = document.querySelector('#eight')
+const nineButton = document.querySelector('#nine')
+const acButton = document.querySelector('#clearAll')
+const delButton = document.querySelector('#delete')
+const mulButton = document.querySelector('#multiplication')
+const divButton = document.querySelector('#division')
+const addButton = document.querySelector('#addition')
+const subButton = document.querySelector('#subtraction')
+const decimalButton = document.querySelector('#decimal')
+const equalButton = document.querySelector('#equals')
+const display = document.querySelector('.display')
+
+acButton.addEventListener('click', () => { display.textContent = 0; });
+
 
 function add(value, operationalValue) {
     return value + operationalValue
